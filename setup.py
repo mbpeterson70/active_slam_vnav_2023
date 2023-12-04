799efd0489d56b52431e26e2ca4637d2533b4488
Mason Peterson <mbpeterson70@gmail.com>
2023-12-04 14:49:17 -0500

    update setup.py with required modules
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

--- setup.py
+++ setup.py
@@ -6,6 +6,14 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['active_slam', 'active_slam.segment_slam'],
     package_dir={'': 'src'},
+    install_requires=[  
+        'numpy',
+        'scipy',
+        'pyproj',
+        'imageio',
+        'scikit-image',
+        'rasterio'
+    ],
 )
 
 setup(**setup_args)
